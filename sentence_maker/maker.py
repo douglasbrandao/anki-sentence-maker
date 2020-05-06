a41a5fda1440c228b0ede00310d4c0adb4c3ec18
Douglas Brandao <douglasfrb@gmail.com>
2020-05-06 15:57:20 -0400

    Updated README

--- sentence_maker/maker.py
+++ sentence_maker/maker.py
@@ -29,7 +29,7 @@ class SentenceMaker:
             phonetic = self.get_phonetic_notation_from_list(word_to_list)
             full_phonetic_notation = '/{}/'.format(phonetic)
 
-        definitions = [s.text for s in soup.find_all('span', class_='def')]
+        definitions = [s.text.strip() for s in soup.find_all('span', class_='def')]
         examples = [s.text for s in soup.select('ul.examples > li > span.x')]
 
         if not examples:
@@ -62,7 +62,7 @@ class SentenceMaker:
             phonetic = self.get_phonetic_notation_from_list(word_to_list)
             full_phonetic_notation = '/{}/'.format(phonetic)
 
-        definitions = [s.text for s in soup.find_all('div', class_='def ddef_d db')]
+        definitions = [s.text.strip().replace(':', '') for s in soup.find_all('div', class_='def ddef_d db')]
         examples = [s.text for s in soup.find_all('div', class_='examp dexamp')]
 
         dataset_examples = soup.find('div', attrs={'id': 'dataset-example'})
@@ -74,7 +74,7 @@ class SentenceMaker:
             raise IndexError(f"We could not find a good amount of examples of [{word}]. Let me try the next one!")
 
         print(Fore.GREEN + Style.BRIGHT + "[WE FOUND IT ON CAMBRIDGE!] -> " + Style.RESET_ALL, end='')
-        print(f'We have found [{word}] on Oxford!')
+        print(f'We have found [{word}] on Cambridge!')
 
         return {
             'name': name,
