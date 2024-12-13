0fa2cb5d37da5e33600d3d1610e084c153a8eebe
Douglas Brandão <douglasfrb@gmail.com>
2024-12-12 23:03:35 -0400

    fix: lint errors

--- anki_sentence_maker/datasources/cambridge.py
+++ anki_sentence_maker/datasources/cambridge.py
@@ -1,39 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
 from anki_sentence_maker.bases import ScrapeDataSource
 from anki_sentence_maker.headers import headers
-from bs4 import BeautifulSoup
 from exceptions import IncorrectlyTypedException
 from type import Data
 from utils import (
     get_phonetic_notation_from_list,
-    get_word_separated_by_delimiter
+    get_word_separated_by_delimiter,
 )
 
-import requests
-
-url = 'https://dictionary.cambridge.org/dictionary/english/'
+url = "https://dictionary.cambridge.org/dictionary/english/"
 
 
 class Cambridge(ScrapeDataSource):
     def scrape(self):
         """Scrape the cambridge dictionary"""
         word_in_kebab_case: str = get_word_separated_by_delimiter(
-            self.word, '-'
+            self.word, "-"
         )
         response = requests.get(url + word_in_kebab_case, headers=headers)
 
         if "Meaning of" not in response.text:
             raise IncorrectlyTypedException(
-                Cambridge.get_classname(),
-                word_in_kebab_case
+                Cambridge.get_classname(), word_in_kebab_case
             )
 
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, "html.parser")
 
-        title = soup.find(attrs={'class': 'di-title'})
-        phonetic_notation = soup.select('span.pron.dpron', limit=1)
-        definitions = soup.find_all('div', class_='def ddef_d db')
-        examples = soup.find_all('div', class_='examp dexamp')
-        dataset_examples = soup.find('div', attrs={'id': 'dataset-example'})
+        title = soup.find(attrs={"class": "di-title"})
+        phonetic_notation = soup.select("span.pron.dpron", limit=1)
+        definitions = soup.find_all("div", class_="def ddef_d db")
+        examples = soup.find_all("div", class_="examp dexamp")
+        dataset_examples = soup.find("div", attrs={"id": "dataset-example"})
 
         try:
             phonetic_notation = phonetic_notation[0].text
@@ -41,21 +40,19 @@ class Cambridge(ScrapeDataSource):
             word_to_list = self.word.split()
             phonetic_notation = get_phonetic_notation_from_list(word_to_list)
 
-        definitions = [d.text.strip().replace(':', '') for d in definitions]
+        definitions = [d.text.strip().replace(":", "") for d in definitions]
         examples = [e.text.strip().capitalize() for e in examples]
 
         if dataset_examples:
             examples.extend(
                 [
-                    e.text.strip().capitalize() for e in soup.find_all(
-                        'span',
-                        class_='deg'
-                    )
+                    e.text.strip().capitalize()
+                    for e in soup.find_all("span", class_="deg")
                 ]
             )
 
         return Data(
-            name=title.text if title else '',
+            name=title.text if title else "",
             phonetic_notation=phonetic_notation,
             definitions=definitions,
             examples=examples,
--- logger/__init__.py
+++ logger/__init__.py
@@ -8,7 +8,7 @@ handler = logging.StreamHandler()
 
 formatter = logging.Formatter(
     "%(name)s - %(levelname)s - %(asctime)s - %(message)s",
-    datefmt="%d/%m/%Y %H:%M:%S"
+    datefmt="%d/%m/%Y %H:%M:%S",
 )
 handler.setFormatter(formatter)
 
